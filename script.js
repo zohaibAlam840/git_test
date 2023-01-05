50d6962a6e2909cd3ff986b1cf68c8a651922b33
zohaib <ikillar840@gmail.com>
2023-01-05 16:43:24 +0500

    index.htlm

--- script.js
+++ script.js
@@ -1,29 +1,71 @@
 let divContainer = document.querySelector(".gridContainer");
 let divArray = new Array();
+
+// grid button varables
 let button = document.querySelectorAll('#grid');
 let gridbtn = Array.from(button);
 let root = document.querySelector(':root');
 let rootstyle = getComputedStyle(root);
+
+// color button varables
+let colorBtn = document.querySelectorAll('#color');
+let colorBtnArray = Array.from(colorBtn);
+let setColor = 'background-color: black;';
+let colorPicker = document.querySelector('#piker');
 function divCreator(divNumber){
     let number=divNumber*divNumber;
     for(let i=0; i<number;i++){
         divArray[i] = document.createElement('div');
         divArray[i].setAttribute('id',i);
         divContainer.appendChild(divArray[i]);
-    }
+    }  
+    selectColor();
+    setColorPiker();
+
 }
 
 gridbtn.forEach(function(item){
     item.addEventListener('click', (e)=> {
+        gridClener();
         root.style.setProperty('--divRC',item.value);
         divCreator(item.value); 
-        let val = item.value; 
-        if(val==0){
-            gridClener();
-        }
     })
     
 })
+
 function gridClener(){
     divContainer.innerHTML=''; 
+}
+function setColorPiker(){
+    divArray.forEach((items) => {
+        items.addEventListener('mouseenter', () => {
+             items.setAttribute('style',setColor);
+             
+          });
+     })
+
+}
+function selectColor(){
+    let selectedColor;
+    colorBtnArray.forEach(function(item){
+        item.addEventListener('click',()=>{
+            if(item.value==1){
+                selectedColor='background-color: white;'
+            }
+            else if(item.value==2){
+                selectedColor='background-color: black;';
+            }
+            else if(item.value==3){
+                let randomColor = colorPicker.value;
+                console.log(randomColor);
+                selectedColor = 'background-color:'+randomColor+';';
+                console.log(selectedColor);
+                  
+            }          
+            setColor=selectedColor;
+            console.log(setColor);
+        })
+        
+    })
+    
 }
